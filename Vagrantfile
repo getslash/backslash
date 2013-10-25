6138e510caec1b9de54417a9c10c0ecb58d4199a
Rotem Yaari <vmalloc@gmail.com>
2013-10-25 22:07:37 +0300

    Remove vagrant deployment

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,5 @@ Vagrant::Config.run do |config|
     config.vm.box_url = "http://dl.dropbox.com/u/1537815/precise64.box"
     config.vm.host_name = "server"
     config.vm.forward_port 80, 8080
-    config.vm.provision :shell, :path => "scripts/_internal_vagrant_setup.sh"
   end
 end
--- scripts/_internal_vagrant_setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -x
-
-cd /vagrant/
-
-sudo apt-get update
-sudo apt-get -y install build-essential python-dev libevent-dev python-pip
-sudo pip install --use-mirrors -r base_requirements.txt
-sudo pip install --use-mirrors -r flask_app/pip_requirements.txt
-make local_deploy
