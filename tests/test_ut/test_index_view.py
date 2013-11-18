a14a29b7cd8582d4e8799b8d5008b0c96bc64c15
Rotem Yaari <vmalloc@gmail.com>
2013-11-18 10:21:45 +0200

    Fix error pages

--- tests/test_ut/test_index_view.py
+++ tests/test_ut/test_index_view.py
@@ -1,6 +1,13 @@
+import httplib
+
 from ..test_utils import TestCase
 
 class IndexViewTest(TestCase):
+
     def test_index_view(self):
         rv = self.app.get("/")
+
+    def test_not_found_errors(self):
+        rv = self.app.get("/nonexistent_path")
+        self.assertEquals(rv.status_code, httplib.NOT_FOUND)
 
