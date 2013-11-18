abd4d5dd4dbaa9164e27d504d9dc7dce354d67ce
Rotem Yaari <vmalloc@gmail.com>
2013-11-18 11:56:54 +0200

    Output emitted data in request helpers

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -18,12 +18,16 @@ def _make_request_shortcut(method):
     def shortcut(self, *args, **kwargs):
         returned = getattr(self.app, method)(*args, **kwargs)
         if returned.status_code not in (httplib.OK, httplib.CREATED):
-            msg = "HTTP {} {}: Error {}".format(method.upper(), args[0], returned.status_code)
+            msg = "HTTP {} {}: Error {} ({})".format(method.upper(), args[0], returned.status_code, _truncate_repr(returned.data))
             self.fail(msg)
         return returned
 
     shortcut_name = shortcut.__name__ = "_{}".format(method)
     setattr(TestCase, shortcut_name, shortcut)
+
+def _truncate_repr(s, length=50):
+    s = repr(s)
+    return "{}...{}".format(s[:length-4], s[0]) if len(s) > length else s
 
 for _method in ("get", "put", "post", "delete"):
     _make_request_shortcut(_method)
