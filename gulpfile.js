5b7b91404f51ef63ec845541aa8ffd84acd34bcd
Rotem Yaari <vmalloc@gmail.com>
2014-03-30 16:50:54 +0300

    Better separation of production from debug builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var argv = require('minimist')(process.argv.slice(2));
+
 var gulp = require("gulp"),
     es6 = require("gulp-es6-module-transpiler"),
     handlebars = require("gulp-ember-handlebars"),
@@ -9,9 +11,9 @@ var gulp = require("gulp"),
     concat = require("gulp-concat-sourcemap");
 
 gulp.task("css", function() {
-    gulp.src('webapp/styles/style.scss')
+    return gulp.src('webapp/styles/style.scss')
         .pipe(sass())
-        .pipe(minifyCSS())
+        .pipe(gulpif(argv.production, minifyCSS()))
         .pipe(rename("app.css"))
         .pipe(gulp.dest('static/css/'));
 });
@@ -37,16 +39,6 @@ gulp.task('templates', function(){
 
 gulp.task("default", ["javascripts", "templates", "css"], function() {
 
-    return _build(false);
-});
-
-gulp.task("prod", ["javascripts", "templates", "css"], function() {
-
-    return _build(true);
-});
-
-
-function _build(production) {
     return gulp.src([
         "bower_components/requirejs/require.js",
         "bower_components/jquery/jquery.js",
@@ -55,10 +47,10 @@ function _build(production) {
         "static/js/_build/templates.js",
         "static/js/_build/app.js"
     ])
-        .pipe(gulpif(production, uglify()))
+        .pipe(gulpif(argv.production, uglify()))
         .pipe(concat("app.js", {sourceRoot: "/"}))
         .pipe(gulp.dest("static/js"));
-}
+});
 
 gulp.task("watch", ["default"], function() {
     gulp.watch(["gulpfile.js", "webapp/**/*.js", "webapp/**/*.hbs"], ["default"]);
