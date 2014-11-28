a9c9031ad099e7a4245766461875ca700bc4fd27
boazin <boazin@gmail.com>
2014-11-28 11:46:47 +0200

    bug fix in link-to + test html template

--- webapp/app/routes/test.js
+++ webapp/app/routes/test.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.get('store').find('test', params.test_id);
+    return this.store.find('test', params.test_id);
   }
 });
