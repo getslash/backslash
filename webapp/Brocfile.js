2db0738af255989a7fbb6348b674580c78f863d6
Boaz Ackerman <boaz@cookila.com>
2015-03-01 17:38:27 +0200

    un-needed commented code

--- webapp/Brocfile.js
+++ webapp/Brocfile.js
@@ -2,11 +2,7 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp(/*{
-  vendorFiles: {
-    'handlebars.js': null
-  }
-}*/);
+var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
