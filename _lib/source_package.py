34fa88b8c1ede6e33404715d85184957919b6415
Rotem Yaari <vmalloc@gmail.com>
2015-03-06 23:39:52 +0200

    Narrow the list of files added to source package to git tracked only + static

--- _lib/source_package.py
+++ _lib/source_package.py
@@ -22,9 +22,10 @@ def _get_paths_to_tar():
     p = subprocess.Popen("git ls-files", stdout=subprocess.PIPE, cwd=from_project_root(), shell=True)
     returned = set()
     for subpath in p.stdout.readlines():
+        if subpath.startswith('webapp/'):
+            continue
         subpath = subpath.decode('UTF-8')
         subpath = subpath.strip()
-        returned.add(os.path.dirname(subpath))
         returned.add(subpath)
     if 0 != p.wait():
         raise RuntimeError("git ls-files failed")
