dce9adabb1c3b1eee8416c564b9e3e08f244073e
Roey Darwish Dror <roeyd@infinidat.com>
2015-03-16 17:43:36 +0200

    Integrate Flask-Security

---
+++ flask_app/setup.py
@@ -0,0 +1,38 @@
+from flask import render_template, abort, redirect
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+from .app import app
+from .models import user_datastore
+
+
+class MyForm(Form):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField(
+        'password', validators=[DataRequired(), EqualTo("confirm_password", "Passwords must match")])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired()])
+
+
+def _has_users():
+    return user_datastore.user_model.query.count() > 0
+
+
+@app.route("/setup", methods=['POST'])
+def setup_submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        if _has_users():
+            abort(403)
+
+        user_datastore.create_user(email=form.email.data, password=form.password.data)
+        user_datastore.db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("setup.html", has_users=_has_users(), form=form, errors=form.errors)
+
+
+@app.route("/setup", methods=['GET'])
+def setup():
+    form = MyForm()
+    return render_template("setup.html", has_users=_has_users(), form=form, errors={})
