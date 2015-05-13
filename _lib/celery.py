12f7ecf65342fa0843c7a89e7620f17284eafea2
Rotem Yaari <vmalloc@gmail.com>
2015-05-13 13:58:38 +0300

    Fix module dependency

--- _lib/celery.py
+++ _lib/celery.py
@@ -2,8 +2,6 @@ import click
 
 import logbook
 
-from flask_app import tasks
-from flask_app.app import create_app
 from .bootstrapping import requires_env
 
 _logger = logbook.Logger(__name__)
@@ -17,8 +15,14 @@ def celery():
 @click.argument('name')
 @requires_env('app')
 def task(name):
+
+    from flask_app import tasks
+    from flask_app.app import create_app
+
+
     task = getattr(tasks, name, None)
     if task is None:
-        click.abort('Could not find task named {}'.format(task))
+        click.echo('Could not find task named {}'.format(task))
+        raise click.Abort()
     with create_app().app_context():
         task()
