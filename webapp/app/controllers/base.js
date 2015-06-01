6c631324e19958bf60a0bc9f7e4da5bf81e33078
Rotem Yaari <vmalloc@gmail.com>
2015-06-01 12:58:01 +0300

    Remove unnecessary log

--- webapp/app/controllers/base.js
+++ webapp/app/controllers/base.js
@@ -5,7 +5,6 @@ export default Ember.Controller.extend({
     gravatarURL: function() {
 
         let email = this.get('session.content.user_info.email');
-        console.log('Getting gravatar for' + email);
         return 'http://www.gravatar.com/avatar/' + window.md5(email);
 
     }.property('session.content.user_info.email')
