807d1c8975da302ef2786bd961c55349e350709f
Rotem Yaari <vmalloc@gmail.com>
2015-06-02 10:02:55 +0300

    Session navigation with tabs

--- webapp/app/session/controller.js
+++ webapp/app/session/controller.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+    needs: ["application"],
+
     currentPath: function() {
 
         return this.controllerFor('application').get('currentPath');
