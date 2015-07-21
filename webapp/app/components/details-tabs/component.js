911295c6b9303efb4c5ea7b3349e92db3bb4f162
Rotem Yaari <vmalloc@gmail.com>
2015-07-21 21:28:15 +0300

    Fix session tabs

--- webapp/app/components/details-tabs/component.js
+++ webapp/app/components/details-tabs/component.js
@@ -8,14 +8,22 @@ export default Ember.Component.extend({
         return this.get('obj.constructor.modelName');
     }.property('obj'),
 
+    has_tests: function() {
+        return this.get('objtype') === 'session';
+    }.property('objtype'),
+
     index_route: function() {
         return this.get('objtype') + '.index';
     }.property('objtype'),
 
-
     errors_route: function() {
         return this.get('objtype') + '.errors';
     }.property('objtype'),
+
+    tests_route: function() {
+        return this.get('objtype') + '.tests';
+    }.property('objtype'),
+
 
     comments_route: function() {
         return this.get('objtype') + '.comments';
