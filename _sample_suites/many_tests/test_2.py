36f01feab0045a42457bb31908fe7b9480dbdc0c
Rotem Yaari <vmalloc@gmail.com>
2015-07-21 21:28:08 +0300

    Add sample suite with many tests

---
+++ _sample_suites/many_tests/test_2.py
@@ -0,0 +1,300 @@
+def test_0():
+    pass
+
+def test_1():
+    pass
+
+def test_2():
+    pass
+
+def test_3():
+    pass
+
+def test_4():
+    pass
+
+def test_5():
+    pass
+
+def test_6():
+    pass
+
+def test_7():
+    pass
+
+def test_8():
+    pass
+
+def test_9():
+    pass
+
+def test_10():
+    pass
+
+def test_11():
+    pass
+
+def test_12():
+    pass
+
+def test_13():
+    pass
+
+def test_14():
+    pass
+
+def test_15():
+    pass
+
+def test_16():
+    pass
+
+def test_17():
+    pass
+
+def test_18():
+    pass
+
+def test_19():
+    pass
+
+def test_20():
+    pass
+
+def test_21():
+    pass
+
+def test_22():
+    pass
+
+def test_23():
+    pass
+
+def test_24():
+    pass
+
+def test_25():
+    pass
+
+def test_26():
+    pass
+
+def test_27():
+    pass
+
+def test_28():
+    pass
+
+def test_29():
+    pass
+
+def test_30():
+    pass
+
+def test_31():
+    pass
+
+def test_32():
+    pass
+
+def test_33():
+    pass
+
+def test_34():
+    pass
+
+def test_35():
+    pass
+
+def test_36():
+    pass
+
+def test_37():
+    pass
+
+def test_38():
+    pass
+
+def test_39():
+    pass
+
+def test_40():
+    pass
+
+def test_41():
+    pass
+
+def test_42():
+    pass
+
+def test_43():
+    pass
+
+def test_44():
+    pass
+
+def test_45():
+    pass
+
+def test_46():
+    pass
+
+def test_47():
+    pass
+
+def test_48():
+    pass
+
+def test_49():
+    pass
+
+def test_50():
+    pass
+
+def test_51():
+    pass
+
+def test_52():
+    pass
+
+def test_53():
+    pass
+
+def test_54():
+    pass
+
+def test_55():
+    pass
+
+def test_56():
+    pass
+
+def test_57():
+    pass
+
+def test_58():
+    pass
+
+def test_59():
+    pass
+
+def test_60():
+    pass
+
+def test_61():
+    pass
+
+def test_62():
+    pass
+
+def test_63():
+    pass
+
+def test_64():
+    pass
+
+def test_65():
+    pass
+
+def test_66():
+    pass
+
+def test_67():
+    pass
+
+def test_68():
+    pass
+
+def test_69():
+    pass
+
+def test_70():
+    pass
+
+def test_71():
+    pass
+
+def test_72():
+    pass
+
+def test_73():
+    pass
+
+def test_74():
+    pass
+
+def test_75():
+    pass
+
+def test_76():
+    pass
+
+def test_77():
+    pass
+
+def test_78():
+    pass
+
+def test_79():
+    pass
+
+def test_80():
+    pass
+
+def test_81():
+    pass
+
+def test_82():
+    pass
+
+def test_83():
+    pass
+
+def test_84():
+    pass
+
+def test_85():
+    pass
+
+def test_86():
+    pass
+
+def test_87():
+    pass
+
+def test_88():
+    pass
+
+def test_89():
+    pass
+
+def test_90():
+    pass
+
+def test_91():
+    pass
+
+def test_92():
+    pass
+
+def test_93():
+    pass
+
+def test_94():
+    pass
+
+def test_95():
+    pass
+
+def test_96():
+    pass
+
+def test_97():
+    pass
+
+def test_98():
+    pass
+
+def test_99():
+    pass
+
