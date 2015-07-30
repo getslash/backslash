e435d39fc18496ba3576c7cd003127951d82cbdd
Rotem Yaari <vmalloc@gmail.com>
2015-07-30 23:51:21 +0300

    Fix tests

--- tests/test_ut/test_archiving.py
+++ tests/test_ut/test_archiving.py
@@ -1,11 +1,19 @@
 from .utils import raises_not_found
 
 def test_archive_session(client, ended_session):
-    ended_session.archive()
+    ended_session.toggle_archived()
     for session in client.query_sessions():
         assert session.id != ended_session.id
 
+def test_archive_unarchive_session(client, ended_session):
+
+    ended_session.toggle_archived()
+    assert ended_session.refresh().archived
+    ended_session.toggle_archived()
+    assert not ended_session.refresh().archived
+
+
 def test_archive_nonexistent_session(client, nonexistent_session):
     with raises_not_found():
-        nonexistent_session.archive()
+        nonexistent_session.toggle_archived()
 
