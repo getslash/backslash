929d209a16634faf3ca9799c59993bcd7e9e4685
Roey Darwish Dror <roeyd@infinidat.com>
2015-08-09 16:56:43 +0300

    systemd fixes

--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 
+from logging import Formatter
+from logbook.compat import LoggingHandler
+from logging.handlers import SysLogHandler
 from celery import Celery
+from celery.signals import after_setup_task_logger, after_setup_logger
+import os
+
 
 queue = Celery('tasks', broker='redis://localhost')
 queue.conf.update(
@@ -9,3 +15,17 @@ queue.conf.update(
     CELERY_RESULT_SERIALIZER='json',
     CELERY_ENABLE_UTC=True,
 )
+
+def setup_log(**args):
+    if os.path.exists("/dev/log"):
+        hl = SysLogHandler('/dev/log')
+        hl.setLevel(args['loglevel'])
+        formatter = Formatter("%(message)s")
+        hl.setFormatter(formatter)
+        args['logger'].addHandler(hl)
+
+    LoggingHandler().push_application()
+
+
+after_setup_logger.connect(setup_log)
+after_setup_task_logger.connect(setup_log)
