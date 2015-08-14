ecd723c7625bf8bffde227d6abc3f91b1dab9c99
Rotem Yaari <vmalloc@gmail.com>
2015-08-14 07:53:46 +0300

    Styling for investigated sessions

--- webapp/app/components/session-row/component.js
+++ webapp/app/components/session-row/component.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
     classNames: "row row-fluid session-row",
+    classNameBindings: ['session.investigated:investigated'],
+
     session: null,
 
     click: function() {
