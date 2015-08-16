4d5f75e82828f8a7984c0b861127b4b75d6be2f9
Rotem Yaari <vmalloc@gmail.com>
2015-08-16 08:34:02 +0300

    Remove unused code

--- webapp/app/index/route.js
+++ webapp/app/index/route.js
@@ -2,11 +2,4 @@ import BaseRoute from '../routes/base';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default BaseRoute.extend(AuthenticatedRouteMixin, {
-  model: function() {
-        return {features: [
-            {name: "Font Awesome", ok: true},
-            {name: "Ember.js", ok: true},
-            {name: "Bootstrap", ok: true}
-        ]};
-    }
 });
