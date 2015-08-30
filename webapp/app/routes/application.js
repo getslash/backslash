62482e7b195be970168062d6e74cde52ff753a34
Rotem Yaari <vmalloc@gmail.com>
2015-08-30 21:43:40 +0300

    wip

--- webapp/app/routes/application.js
+++ webapp/app/routes/application.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
-import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
-
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
 });
