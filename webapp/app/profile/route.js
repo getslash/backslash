a740987db33fada703de5e8abeebfb2cdf164ea7
Rotem Yaari <vmalloc@gmail.com>
2015-10-01 15:41:54 +0300

    Styling fixes

--- webapp/app/profile/route.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-
-    beforeModel: function() {
-        this.transitionTo('user', this.get('session.content.user_info.user_id'));
-    }
-});
