f72996661d5d557aa085a47adb919c93155764d2
Rotem Yaari <vmalloc@gmail.com>
2015-10-16 23:02:15 +0300

    Loading indicator ng

--- webapp/app/application/route.js
+++ webapp/app/application/route.js
@@ -3,13 +3,19 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
 
-
     actions: {
         goto_session: function (session) {
             this.transitionTo('session', session);
         },
         goto_test: function(test) {
             this.transitionTo('test', test);
+        },
+        loading: function(transition) {
+            let controller = this.controllerFor('application');
+            controller.set('loading', true);
+            transition.promise.finally(function() {
+                controller.set('loading', false);
+            });
         }
     }
 });
