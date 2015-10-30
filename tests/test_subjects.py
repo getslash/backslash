c254c6d087a6fb05d4de5223829d94a0cb7ef96f
Rotem Yaari <vmalloc@gmail.com>
2015-10-30 13:37:06 +0200

    Pylint fixes

--- tests/test_subjects.py
+++ tests/test_subjects.py
@@ -9,16 +9,20 @@ def test_add_subject(started_session, subjects, flask_app):
         session = model_for(started_session)
         assert len(session.subject_instances) == 1
         assert session.subject_instances[0].subject.name == subjects[0]['name']
-        assert session.subject_instances[0].revision.product_version.product.name == subjects[0]['product']
+        assert session.subject_instances[
+            0].revision.product_version.product.name == subjects[0]['product']
+
 
 def test_product_rendered_field(started_session, subjects):
     started_session.add_subject(**subjects[0])
     started_session.refresh()
     assert started_session.subjects == [subjects[0]]
 
+
 @pytest.mark.parametrize('field_name', ['product', 'version', 'revision'])
 def test_add_subject_deduplication(started_session, flask_app, field_name):
-    first_subject = {'name': 'some_subject', 'product': 'car', 'version': '1', 'revision': 'a'}
+    first_subject = {'name': 'some_subject',
+                     'product': 'car', 'version': '1', 'revision': 'a'}
     started_session.add_subject(**first_subject)
     second_subject = first_subject.copy()
     second_subject[field_name] = 'new_field_value'
@@ -27,10 +31,9 @@ def test_add_subject_deduplication(started_session, flask_app, field_name):
     with flask_app.app_context():
         session = model_for(started_session)
         assert len(session.subject_instances) == 2
-        s1, s2 = session.subject_instances
-        prod1, prod2 = map(lambda x: x.revision.product_version.product.id, session.subject_instances)
-        ver1, ver2 = map(lambda x: x.revision.product_version.id, session.subject_instances)
-        rev1, rev2 = map(lambda x: x.revision.id, session.subject_instances)
+        prod1, prod2 = [x.revision.product_version.product.id for x in session.subject_instances]
+        ver1, ver2 = [x.revision.product_version.id for x in session.subject_instances]
+        rev1, rev2 = [x.revision.id for x in session.subject_instances]
 
         if field_name == 'product':
             assert prod1 != prod2
@@ -45,4 +48,4 @@ def test_add_subject_deduplication(started_session, flask_app, field_name):
             assert ver1 == ver2
             assert rev1 != rev2
         else:
-            raise NotImplementedError() # pragma: no cover
+            raise NotImplementedError()  # pragma: no cover
