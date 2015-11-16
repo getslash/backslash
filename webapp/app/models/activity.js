95078dc07c4f6e08d688552dcfc8df6d754b5b5e
Rotem Yaari <vmalloc@gmail.com>
2015-11-16 23:14:47 +0200

    Remove redundant code

--- webapp/app/models/activity.js
+++ webapp/app/models/activity.js
@@ -4,39 +4,6 @@ export default DS.Model.extend({
     action: DS.attr(),
     timestamp: DS.attr(),
     user_email: DS.attr(),
-    text: DS.attr(),
-
-    message: function() {
-
-        const what = this.get('what');
-        switch (this.get('action_string')) {
-
-            case 'archived': return 'archived this ' + what;
-            case 'unarchived': return 'unarchived this ' + what;
-            case 'investigated': return 'marked this ' + what + ' as investigated';
-            case 'uninvestigated': return 'marked this ' + what + ' as not investigated';
-            default: return '???';
-        }
-
-    }.property('action_string'),
-
-    icon: function() {
-        switch (this.get('action_string')) {
-            case 'archived':
-            case 'unarchived':
-                return 'archive';
-            case 'investigated':
-                return 'check';
-            case 'uninvestigated':
-                return 'times';
-            case 'commented':
-                return 'comment';
-        }
-        return 'bullseye';
-    }.property('action_string'),
-
-    activity_icon_class: function() {
-        return this.get('action_string');
-    }.property('action_string')
+    text: DS.attr()
 
 });
