5e516522236a5dd831e060c67fee2ac7cf4c9158
Rotem Yaari <vmalloc@gmail.com>
2016-02-04 23:37:01 +0200

    Better error page

--- webapp/app/adapters/application.js
+++ webapp/app/adapters/application.js
@@ -8,13 +8,12 @@ export default DS.RESTAdapter.extend({
       var plural = Ember.String.pluralize(type);
       return Ember.String.underscore(plural);
   },
-  
+
   ajax: function(url, type, options) {
     return this._super(url, type, options).catch(function(error){
     	error.errors.forEach(function(e){
     		if (e.status==="404"){ // Replace Ember's default message for 404
-    			e.title="Page not found"; 
-    			error.message = "This is not the page you are looking for...";
+                    error.not_found = true;
     		}
     	});
     	throw error;
