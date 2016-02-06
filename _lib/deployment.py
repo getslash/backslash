ec19fd5f90e110347f58a63e2773d874054c6515
Rotem Yaari <vmalloc@gmail.com>
2016-02-06 23:00:19 +0200

    Properly pass number of workers to gunicorn

--- _lib/deployment.py
+++ _lib/deployment.py
@@ -13,7 +13,8 @@ _UNIX_SOCKET_NAME = "/var/run/{}/wsgi.sock".format(APP_NAME)
 @click.command()
 @requires_env("app")
 def run_gunicorn():
+    num_workers = (2 * cpu_count()) + 1
     gunicorn_bin = from_env_bin('gunicorn')
-    cmd = [gunicorn_bin, '--log-syslog', '-b', 'unix://{}'.format(_UNIX_SOCKET_NAME), 'flask_app.wsgi:app', '--chdir', from_project_root('.')]
+    cmd = [gunicorn_bin, '--log-syslog', '-b', 'unix://{}'.format(_UNIX_SOCKET_NAME), 'flask_app.wsgi:app', '--chdir', from_project_root('.'), '-w', str(num_workers)]
     os.execv(gunicorn_bin, cmd)
 
