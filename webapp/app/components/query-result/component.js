d9942d3720be3e8c5523b1f9fcb3a0c5ae3e09b7
Rotem Yaari <vmalloc@gmail.com>
2016-03-07 23:42:49 +0200

    Condense test rows

--- webapp/app/components/query-result/component.js
+++ webapp/app/components/query-result/component.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    classNames: ['container-fluid', 'query-result', 'test', 'clickable'],
-    classNameBindings: ['result.investigated:investigated', 'result.is_abandoned:abandoned'],
+    classNames: ['container-fluid', 'query-result', 'clickable'],
+    classNameBindings: ['result.investigated:investigated', 'result.is_abandoned:abandoned', 'result_type'],
     result: null,
+
+    result_type: Ember.computed.oneWay('result.type'),
 
     typename: Ember.computed.oneWay('result.type'),
 
