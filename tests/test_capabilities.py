71dc1f867bc9d207ca31406e95373c9e80ac881b
Rotem Yaari <vmalloc@gmail.com>
2016-03-08 12:30:01 +0200

    Fix broken test

--- tests/test_capabilities.py
+++ tests/test_capabilities.py
@@ -1,3 +1,5 @@
-def test_user_capabilities(client, real_login):
-    caps = client.api.get('/rest/users/self')['user']['capabilities']
+# pylint: disable=unused-argument
+
+def test_user_capabilities(client, real_login, testuser_id):
+    caps = client.api.get('/rest/users/{}'.format(testuser_id))['user']['capabilities']
     assert caps['comment_test'] is True
