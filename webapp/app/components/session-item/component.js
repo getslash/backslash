348fd8f29e0be7d8a4722d0bc6e985be1e2da218
Rotem Yaari <vmalloc@gmail.com>
2016-03-22 10:39:11 +0200

    Handling of sessions without finished tests is same as abandoned now

--- webapp/app/components/session-item/component.js
+++ webapp/app/components/session-item/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
     tagName: 'a',
     attributeBindings: ['href'],
     classNames: ['item', 'session', 'clickable'],
-    classNameBindings: ['item.investigated:investigated', 'item.is_abandoned:abandoned', 'result_type', 'status'],
+    classNameBindings: ['item.investigated:investigated', 'is_abandoned:abandoned', 'result_type', 'status'],
 
     session: Ember.computed.oneWay('item'),
 
@@ -25,11 +25,23 @@ export default Ember.Component.extend({
                 return 'running';
             } else if (item.get('num_finished_tests')) {
                 return 'success';
+            } else {
+                return 'finished';
             }
         }
     }.property('item.num_skipped_tests', 'item.num_error_tests', 'item.num_failed_tests', 'item.is_running'),
 
     result_type: Ember.computed.oneWay('item.type'),
+
+    abandoned_reason: function() {
+        if (this.get('session.is_abandoned')) {
+            return 'Never completed';
+        } else if (!this.get('session.num_finished_tests') && this.get('session.end_time')) {
+            return 'Ended; No finished tests';
+        }
+    }.property('session.is_abandoned', 'session.num_finished_tests'),
+
+    is_abandoned: Ember.computed.notEmpty('abandoned_reason'),
 
     total_num_unsuccessful: function() {
         let item = this.get('item');
