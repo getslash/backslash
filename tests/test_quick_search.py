a6a72221982d3f75c33bafd2e5654112327a8167
Rotem Yaari <vmalloc@gmail.com>
2016-03-22 10:38:30 +0200

    Fix broken test

--- tests/test_quick_search.py
+++ tests/test_quick_search.py
@@ -1,6 +1,6 @@
 def test_quick_search_subject(client, subjects):
     client.report_session_start(subjects=subjects)
-    res = client.api.call_function('quick_search', {'term': subjects[0]['name'][1:2]})
+    res = client.api.call_function('quick_search', {'term': subjects[0]['name'][1:10]})
     assert subjects[0]['name'] in {x['name'] for x in res if x['type'] == 'subject'}
 
 def test_quick_search_user(client, testuser_email):
