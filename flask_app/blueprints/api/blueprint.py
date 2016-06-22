123a4243473c70ffe9fb2de43c95c2c88b90cfeb
Rotem Yaari <vmalloc@gmail.com>
2016-06-22 09:23:22 +0300

    Add API introspection via OPTIONS /api

--- flask_app/blueprints/api/blueprint.py
+++ flask_app/blueprints/api/blueprint.py
@@ -11,11 +11,18 @@ blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 api = SimpleAPI(blueprint)
 
-def API(func=None, require_real_login=False, generates_activity=True, require_login=True):
+_api_info = {'endpoints': {}}
+
+
+def API(func=None, require_real_login=False, generates_activity=True, require_login=True, version=1):
     if func is None:
-        return functools.partial(API, require_real_login=require_real_login, generates_activity=generates_activity, require_login=require_login)
+        return functools.partial(API, require_real_login=require_real_login, generates_activity=generates_activity, require_login=require_login, version=version)
 
     returned = auto_render(func)
+
+    endpoint_info = _api_info['endpoints'][func.__name__] = {}
+    endpoint_info['login_required'] = require_login
+    endpoint_info['version'] = version
 
     if generates_activity:
         returned = activity.updates_last_active(returned)
@@ -26,3 +33,9 @@ def API(func=None, require_real_login=False, generates_activity=True, require_lo
         else:
             returned = requires_login_or_runtoken(returned)
     return api.include(returned)
+
+
+@blueprint.route('/', methods=['OPTIONS'], strict_slashes=False)
+def get_api_info():
+    from flask import jsonify
+    return jsonify(_api_info)
---
+++ tests/test_api_introspection.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_api_methods(api_info):
+    assert api_info.endpoints.report_test_start.version == 1
+
+
+@pytest.fixture
+def api_info(client):
+    return client.api.info()
