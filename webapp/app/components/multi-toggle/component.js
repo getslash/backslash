b3bedc50df318a21355c34c14e0cf284c9b01b7c
Rotem Yaari <vmalloc@gmail.com>
2016-06-30 23:22:51 +0300

    Add display options to multi-toggle

--- webapp/app/components/multi-toggle/component.js
+++ webapp/app/components/multi-toggle/component.js
@@ -4,6 +4,30 @@ export default Ember.Component.extend({
 
     saving: false,
 
+    display: null,
+    options: null,
+
+    options_and_display: function() {
+
+	let display = this.get('display');
+	let options = this.get('options');
+
+	console.log('display:', display, ', options:', options);
+
+	if (!display) {
+	    display = options;
+	}
+
+	let returned = {};
+
+	for (let i = 0; i < options.length; ++i) {
+	    returned[options[i]] = display[i];
+	}
+	console.log(returned);
+	return returned;
+    }.property('options', 'display'),
+
+
     actions: {
 
 	choose: function(option) {
