4e4fe225c9f964610acaf5f87b59e6f7cafe0ff9
Rotem Yaari <vmalloc@gmail.com>
2016-07-12 22:48:16 +0300

    Fix test history view

--- webapp/app/test_info/controller.js
+++ webapp/app/test_info/controller.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 import PaginatedFilteredController from '../controllers/paginated_filtered_controller';
+import StatusFilterableController from '../mixins/status-filterable/controller';
 
-export default PaginatedFilteredController.extend({
+export default PaginatedFilteredController.extend(StatusFilterableController, {
     page: 1,
 
     collection: Ember.computed.oneWay('tests'),
--- webapp/app/test_info/route.js
+++ webapp/app/test_info/route.js
@@ -1,21 +1,29 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import StatusFilterableRoute from '../mixins/status-filterable/route';
 import PaginatedFilteredRoute from '../routes/paginated_filtered_route';
 
-export default PaginatedFilteredRoute.extend(AuthenticatedRouteMixin, {
+export default PaginatedFilteredRoute.extend(AuthenticatedRouteMixin, StatusFilterableRoute, {
 
     title: 'Test Information',
 
     model(params) {
-        let test_info_id = params.id;
+	let query_params = {
+                info_id: params.id,
+                page: params.page,
+                page_size: params.page_size,
+        };
+
+        for (let key in params) {
+            if (key.startsWith('show_')) {
+                query_params[key] = params[key];
+            }
+        }
+
 
         return Ember.RSVP.hash({
-            tests: this.store.query('test', {
-                info_id: test_info_id,
-                filter: params.filter,
-                page: params.page,
-            }),
-            test_info: this.store.find('test-info', test_info_id),
+            tests: this.store.query('test', query_params),
+            test_info: this.store.find('test-info', params.id),
         });
     },
 
