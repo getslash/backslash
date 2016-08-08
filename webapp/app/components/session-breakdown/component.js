909eb1f62568326626f876c9cd09fbe4da8dff8f
Rotem Yaari <vmalloc@gmail.com>
2016-08-08 15:19:56 +0300

    Better session breakdown display

--- webapp/app/components/session-breakdown/component.js
+++ webapp/app/components/session-breakdown/component.js
@@ -1,99 +1,56 @@
 import Ember from 'ember';
 
-var everything = ['num_failed_tests', 'num_error_tests', 'num_skipped_tests', 'num_finished_tests', 'is_running'];
-
 export default Ember.Component.extend({
-
-    classNames: ['margin-bottom-large'],
 
     session: null,
 
-    tooltip_position: "right",
-
-    total_num_tests: function() {
-        let total = this.get('session.total_num_tests');
-
-        if (total === null) {
-            total = this.get('session.num_finished_tests');
-        }
-        return total;
+    data: function() {
+	const returned = {
+	    columns: this._get_columns(),
+	    type: 'donut',
+	    colors: {
+		failed: '#d9534f',
+		errored: 'red',
+		skipped: '#f0ad4e',
+		successful: '#5cb85c',
+	    }
+	};
+	console.table(returned.columns);
+	return returned;
     }.property('session'),
 
-    num_failed_tests: Ember.computed.alias('session.num_failed_tests'),
-    num_skipped_tests: Ember.computed.alias('session.num_skipped_tests'),
-    num_error_tests: Ember.computed.alias('session.num_error_tests'),
-    num_finished_tests: Ember.computed.alias('session.num_finished_tests'),
-    is_abandoned: Ember.computed.alias('session.is_abandoned'),
+    _get_columns() {
+	let s = this.get('session');
 
-    is_running: function() {
-        return this.get('session.is_running') && !this.get('is_abandoned');
-    }.property('session.is_running', 'session.is_abandoned'),
-
-    num_successful_tests: function() {
-        return this.get('num_finished_tests') - this.get('num_error_tests') - this.get('num_failed_tests') - this.get('num_skipped_tests');
-    }.property('session'),
-
-    tooltip_content: function() {
-        let returned = this.get('total_num_tests') + " tests collected";
-
-        returned += this._add_counter_summary('Successful', 'num_successful_tests', 'success');
-        returned += this._add_counter_summary('Failed', 'num_failed_tests', 'error');
-        returned += this._add_counter_summary('Errored', 'num_error_tests', 'error');
-        returned += this._add_counter_summary('Skipped', 'num_skipped_tests', 'skip');
-
-        const unrun = this.get('total_num_tests') - this.get('num_finished_tests');
-
-        if (unrun) {
-            returned += ' <span class="faint">(' + unrun + ' test' + ((unrun === 1)?'':'s') + ' not run)</span>';
-        }
-
-        return returned;
-
-    }.property('session'),
-
-    _add_counter_summary: function(title, attr, classname=null) {
-        const value = this.get(attr);
-
-        const tag = (classname===null)?'<strong><span>':('<strong><span class="' + classname + '">');
-
-        if (value) {
-            let returned = ', ' + tag + value + '</span></strong> ' + title;
-            return returned;
-        }
-
-        return '';
+	let all = [
+		['failed', s.get('num_failed_tests')],
+		['errored', s.get('num_error_tests')],
+		['skipped', s.get('num_skipped_tests')],
+		['successful', this.get('num_successful_tests')],
+	    ];
+	let returned = []
+	for (let item of all) {
+	    if (item[1]) {
+		returned.push(item);
+	    }
+	}
+	return returned;
     },
 
-    failedPercent: Ember.computed(function() {
-
-        return this._percentOfTotal(parseInt(this.get('num_failed_tests')) + parseInt(this.get('num_error_tests')));
-    }).property(...everything),
-
-    has_errors_or_failures: function() {
-        return this.get('session.num_errors') || this.get('session.num_failures');
+    num_successful_tests: function() {
+        return this.get('session.num_finished_tests') - this.get('session.num_error_tests') - this.get('session.num_failed_tests') - this.get('session.num_skipped_tests');
     }.property('session'),
 
-    successPercent: function() {
+    donut: {
+	title: 'Session breakdown',
+    },
 
-        return this._percentOfTotal(parseInt(this.get('num_finished_tests')) - parseInt(this.get('num_failed_tests')) - parseInt(this.get('num_error_tests')) - parseInt(this.get('num_skipped_tests')));
-    }.property(...everything),
-
-    skipPercent: function() {
-        return this._percentOfTotal(this.get('num_skipped_tests'));
-    }.property(...everything),
-
-    runningPercent: function() {
-        return this._percentOfTotal(1);
-    }.property(...everything),
-
-    _percentOfTotal: function(num) {
-        let total = this.get('total_num_tests');
-        var percentage = 0;
-        if (total !== 0) {
-            percentage = Math.floor((parseInt(num) / total) * 100);
-        }
-
-        return ('width: ' + percentage + '%').htmlSafe();
-    }
+    tooltip: {
+	format: {
+	    value: function(value) {
+		return value;
+	    },
+	},
+    },
 
 });
