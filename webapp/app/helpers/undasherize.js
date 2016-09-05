50b77c5640a0c792d9ecdc8ccf5f8e07ce7a3194
Rotem Yaari <vmalloc@gmail.com>
2016-09-05 21:17:23 +0300

    Fix metadata formatting for dasherized names

--- webapp/app/helpers/undasherize.js
+++ webapp/app/helpers/undasherize.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function undasherize(params/*, hash*/) {
-  return params[0].replace("_", " ");
+  return params[0].replace(/_/g, " ");
 }
 
 export default Ember.Helper.helper(undasherize);
