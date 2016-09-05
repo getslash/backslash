40a4d79b05cdf64ac17060fe505d8923cbf302c7
Rotem Yaari <vmalloc@gmail.com>
2016-09-05 09:48:17 +0300

    Add missing test info to test view

--- webapp/app/session/test/index/controller.js
+++ webapp/app/session/test/index/controller.js
@@ -5,4 +5,19 @@ export default Ember.Controller.extend({
 	return {
 	};
     }.property('test'),
+
+    scm_details: function() {
+	let self = this;
+	let test = self.get('test_model');
+
+	if (!test.get('scm')) {
+	    return {};
+	}
+
+	return Ember.Object.create({
+	    'Revision': test.get('scm_revision'),
+	    'File Hash': test.get('file_hash')
+	});
+
+    }.property('test'),
 });
