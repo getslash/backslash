db488c4b379c00105f02bc00e671752b52b9c489
Rotem Yaari <vmalloc@gmail.com>
2016-09-14 09:54:43 +0300

    Keyboard shortcut for expanding comments

--- webapp/app/components/item-comment-overview/component.js
+++ webapp/app/components/item-comment-overview/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
 
     classNames: ['right-label', 'fainter', 'comments', 'expand-on-hover'],
 
-    classNameBindings: 'visible',
+    classNameBindings: ['visible', 'expanded'],
 
     visible: function() {
 	if (this.get('item.num_comments')) {
