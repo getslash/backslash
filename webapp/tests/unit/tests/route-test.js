6af10d3824beefac78f98bee18eee0720baa4b93
Rotem Yaari <vmalloc@gmail.com>
2016-11-17 11:30:46 +0200

    Fix tests

--- webapp/tests/unit/tests/route-test.js
+++ webapp/tests/unit/tests/route-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:search', 'Unit | Route | search', {
+moduleFor('route:tests', 'Unit | Route | tests', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
