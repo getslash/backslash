c571a8596a5d8da93c990c2a65180c5ada3368ef
Rotem Yaari <vmalloc@gmail.com>
2016-11-29 22:48:28 +0200

    Fix pagination controls

--- webapp/app/components/pagination-nav/component.js
+++ webapp/app/components/pagination-nav/component.js
@@ -5,32 +5,19 @@ export default Ember.Component.extend({
     page: 1,
     has_next: false,
 
-    paginated: function() {
-        return this.get('pages_total') !== 1;
-    }.property('pages_total'),
-
-    
-    has_prev: function() {
-        return this.get('page') !== 1;
-    }.property('page'),
-
-
     actions: {
-        goto: function(page) {
-            let self = this;
-            let p = self.get('page');
-            if (page === "next") {
-                p = self.get('has_next')?(p + 1):p;
-            } else if (page === "prev") {
-                p = self.get('has_prev')?(p - 1):p;
-            } else if (page === 'last') {
-                p = self.get('pages_total');
-            } else if (page === 'first') {
-                p = 1;
-            } else {
-                p = page;
-            }
-            this.sendAction('goto_page', p);
-        }
-    }
+
+	first_page() {
+	    this.set('page', 1);
+	},
+
+	next_page() {
+	    this.incrementProperty('page');
+	},
+
+	prev_page() {
+	    this.decrementProperty('page');
+	},
+
+    },
 });
