be5c0595cc29b932318b2829e4503bf115a639da
Rotem Yaari <vmalloc@gmail.com>
2016-12-07 22:00:33 +0200

    Add log for users which are not found in DB

--- flask_app/utils/users.py
+++ flask_app/utils/users.py
@@ -1,15 +1,19 @@
 from .caching import cache
 from .. import models
 
+import logbook
 from flask import abort
 
 from sqlalchemy.orm.exc import NoResultFound
+
+_logger = logbook.Logger(__name__)
 
 @cache.cache_on_arguments()
 def get_user_id_by_email(email):
     try:
         return models.User.query.filter_by(email=email).one().id
     except NoResultFound:
+        _logger.error('User with email {!r} not found!', email)
         abort(404)
 
 def has_role(user, role):
