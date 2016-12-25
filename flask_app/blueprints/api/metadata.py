2e58ebb6da1f6282ce0b6a32fb0c20b8b7310e3b
Rotem Yaari <vmalloc@gmail.com>
2016-12-25 10:18:41 +0200

    Revert "Improve metadata fetching"
    This reverts commit ad8ce86578f4f5d2bd79309fd73e15d038084830.

--- flask_app/blueprints/api/metadata.py
+++ flask_app/blueprints/api/metadata.py
@@ -1,6 +1,5 @@
 import requests
 
-from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -11,24 +10,20 @@ from flask_simple_api import error_abort
 from ...models import Session, Test, db, SessionMetadata, TestMetadata
 from .blueprint import API
 
-
 @API
 def set_metadata(entity_type: str, entity_id: int, key: str, value: object):
-    _set_metadata_dict(entity_type=entity_type,
-                       entity_id=entity_id, metadata={key: value})
+    _set_metadata_dict(entity_type=entity_type, entity_id=entity_id, metadata={key: value})
 
 
 @API
 def set_metadata_dict(entity_type: str, entity_id: int, metadata: dict):
-    _set_metadata_dict(entity_type=entity_type,
-                       entity_id=entity_id, metadata=metadata)
+    _set_metadata_dict(entity_type=entity_type, entity_id=entity_id, metadata=metadata)
 
 
 def _set_metadata_dict(*, entity_type, entity_id, metadata, commit=True):
     model = _get_metadata_model(entity_type)
     for key, value in metadata.items():
-        db.session.add(model(key=key, metadata_item=value, **
-                             {'{}_id'.format(entity_type): entity_id}))
+        db.session.add(model(key=key, metadata_item=value, **{'{}_id'.format(entity_type): entity_id}))
 
     if commit:
         try:
@@ -37,14 +32,24 @@ def _set_metadata_dict(*, entity_type, entity_id, metadata, commit=True):
             abort(requests.codes.not_found)
 
 
+
 @API(require_login=False)
 def get_metadata(entity_type: str, entity_id: (int, str)):
-    if entity_type not in {'session', 'test'}:
+    query = _get_metadata_query(entity_type=entity_type, entity_id=entity_id)
+    return {obj.key: obj.metadata_item for obj in query}
+
+
+def _get_metadata_query(*, entity_type, entity_id):
+    model = _get_metadata_model(entity_type)
+    if entity_type == 'session':
+        related = Session
+    elif entity_type == 'test':
+        related = Test
+    else:
         error_abort('Invalid entity type', requests.codes.bad_request)
-    query = text(('select json_object_agg(key, metadata_item)'
-                  ' from {0}_metadata'
-                  ' where {0}_id = :entity_id group by {0}_id').format(entity_type))
-    return db.session.execute(query, {'entity_id': entity_id}).scalar()
+    if isinstance(entity_id, int):
+        return model.query.filter_by(**{'{}_id'.format(entity_type): entity_id})
+    return model.query.join(related).filter(related.logical_id == entity_id)
 
 
 def _get_metadata_model(entity_type):
