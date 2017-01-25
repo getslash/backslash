b0fa71d448b833caa54194caa4e588cda4b12358
Rotem Yaari <vmalloc@gmail.com>
2017-01-25 17:45:07 +0200

    Fix loading state

--- webapp/app/loading/route.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-
-    activate() {
-        let c = this.controllerFor('application');
-        c.set('loading', true);
-    }
-});
