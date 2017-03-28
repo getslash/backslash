d59206c1854e845dfaeadaa587688e926ff54dc2
Rotem Yaari <vmalloc@gmail.com>
2017-03-28 21:22:05 +0300

    Separate integration tests to separat directory

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -8,6 +8,12 @@ from urlobject import URLObject
 
 _docker_running = False
 
+
+def pytest_addoption(parser):
+    parser.addoption("--app-url", action="store", default=None,
+                     help="Integration App URL")
+
+
 @pytest.fixture(autouse=True, scope='session')
 def cleanup_docker(request):
     @request.addfinalizer
@@ -18,17 +24,10 @@ def cleanup_docker(request):
 
 @pytest.fixture(scope='session')
 def integration_url(request, timeout=30):
-    start_docker = request.config.getoption('--start-docker')
     url = request.config.getoption("--app-url")
 
-    if start_docker:
-        _start_docker()
-        if url is None:
-            url = 'http://127.0.0.1:8000'
-
     if url is None:
-        pytest.skip('No integration URL provided')
-
+        raise RuntimeError('No integration URL provided')
 
     end_time = time.time() + timeout
     retry = 0
@@ -73,10 +72,12 @@ def _start_docker():
     _run_docker_compose('up -d')
     _docker_running = True
 
+
 def _stop_docker():
     global _docker_running
     _run_docker_compose('down')
     _docker_running = False
+
 
 def _run_docker_compose(cmd):
     subprocess.run(
