aad6efd2be80a76a5bb66d52f8a6ed1872376dbd
Rotem Yaari <vmalloc@gmail.com>
2017-05-15 16:17:49 +0300

    More fixes for 2.13

--- webapp/ember-cli-build.js
+++ webapp/ember-cli-build.js
@@ -3,6 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+
   var app = new EmberApp({
       'ember-font-awesome': {
           useScss: true,
--- webapp/tests/unit/session/info/route-test.js
+++ webapp/tests/unit/session/info/route-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:session/info', 'Unit | Route | session/info', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:api']
 });
 
 test('it exists', function(assert) {
