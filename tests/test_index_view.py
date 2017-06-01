d5ea0f8bcfc66650404726a05c22f43b3749ccbc
Rotem Yaari <vmalloc@gmail.com>
2017-06-01 14:12:30 +0300

    Remove unnecessary test

--- tests/test_index_view.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def test_index_view(webapp):
-    webapp.get_raw("/").raise_for_status()
-
-def test_not_found_errors(webapp):
-    assert webapp.get_raw("/nonexistent_path").status_code == requests.codes.not_found
-
-
