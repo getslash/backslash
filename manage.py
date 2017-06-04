ac39f70432b43117b3f5eb01b9b56919e61674b7
Rotem Yaari <vmalloc@gmail.com>
2017-06-04 17:34:23 +0300

    Fix proxy support in production

--- manage.py
+++ manage.py
@@ -64,11 +64,13 @@ def docker_start():
     from flask_app.models import db
     import flask_migrate
     import gunicorn.app.base
+    from werkzeug.contrib.fixers import ProxyFix
 
 
     _ensure_conf()
 
     app = create_app(config={'PROPAGATE_EXCEPTIONS': True})
+    app.wsgi_app = ProxyFix(app.wsgi_app)
 
     flask_migrate.Migrate(app, db)
 
