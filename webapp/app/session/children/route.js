5440900a79afd8f21e9f4bee16e972b8f25d30ae
Maor Marcus <mmarcus@infinidat.com>
2017-06-18 14:38:04 +0300

    Adding polling_route to children route and fix session route

--- webapp/app/session/children/route.js
+++ webapp/app/session/children/route.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ComplexModelRoute from '../../mixins/complex-model-route';
+import PollingRoute from "../../mixins/polling-route";
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, ComplexModelRoute, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ComplexModelRoute, PollingRoute, {
 
     model: function() {
       let parent_logical_id = this.modelFor('session').session_model.get('logical_id');
@@ -10,5 +11,9 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, ComplexModelRoute, {
       return Ember.RSVP.hash({
         children: this.store.query('session', query_params),
       });
+    },
+    should_auto_refresh: function() {
+      const end_time = this.modelFor("session").session_model.get("end_time");
+      return end_time === null;
     },
 });
