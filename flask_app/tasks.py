e5d557ded5ff6d569534de55c4b8f59bf8d4b51f
Rotem Yaari <vmalloc@gmail.com>
2017-07-17 15:20:03 +0300

    Fix tasks in docker deployments

--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -20,11 +20,8 @@ from . import models
 _logger = logbook.Logger(__name__)
 
 
-queue = Celery('tasks', broker='redis://localhost')
+queue = Celery('tasks', broker=os.environ.get('BACKSLASH_CELERY_BROKER_URL', 'amqp://guest:guest@localhost'))
 queue.conf.update(
-    CELERY_TASK_SERIALIZER='json',
-    CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
-    CELERY_RESULT_SERIALIZER='json',
     CELERY_ENABLE_UTC=True,
     CELERYBEAT_SCHEDULE={
         'live-migration': {
