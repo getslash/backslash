615b5ff66f0e1d4e652cb0a0c694cc82bf1cfe76
Rotem Yaari <vmalloc@gmail.com>
2017-07-31 11:05:41 +0300

    Automatically increment version on new releases

---
+++ scripts/travis_version_fix.py
@@ -0,0 +1,7 @@
+import os
+
+if __name__ == "__main__":
+    travis_tag = os.environ.get('TRAVIS_TAG')
+    if travis_tag:
+        with open('flask_app/__version__.py') as f:
+            print(f'__version__ = "{travis_tag}"', file=f)
