bff338ba80d688c91847aae025c146952d6c82b5
Rotem Yaari <vmalloc@gmail.com>
2018-04-12 10:02:35 +0300

    Make parent_logical_id cascading-delete

--- flask_app/models.py
+++ flask_app/models.py
@@ -118,7 +118,7 @@ class Session(db.Model, TypenameMixin, StatusPredicatesMixin, HasSubjectsMixin, 
     logical_id = db.Column(db.String(256), unique=True, index=True)
     updated_at = db.Column(db.DateTime(), onupdate=datetime.now, index=True, nullable=True)
 
-    parent_logical_id = db.Column(db.String(256), db.ForeignKey('session.logical_id'), default=None, index=True)
+    parent_logical_id = db.Column(db.String(256), db.ForeignKey('session.logical_id', ondelete='CASCADE'), default=None, index=True)
     children = db.relationship('Session', backref=backref('parent', remote_side=[logical_id]))
     is_parent_session = db.Column(db.Boolean, server_default='FALSE')
     child_id = db.Column(db.String(20), default=None)
