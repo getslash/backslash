b03b0bf124c19205800486b37f923b6990f82342
Rotem Yaari <vmalloc@gmail.com>
2018-09-02 17:03:32 +0300

    Handle invalid page sizes, fix error display logic (closes #407)

--- webapp/app/components/request-error-details/component.js
+++ webapp/app/components/request-error-details/component.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
 
 export default Component.extend({
   classNames: ['container'],
   error: null,
+
+  error_string: computed('error', function() {
+    return JSON.stringify(this.get('error'));
+  }),
 });
