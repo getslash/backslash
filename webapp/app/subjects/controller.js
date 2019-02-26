fea8a629bb2717a537abefd81255c7f282b3e47e
Rotem Yaari <vmalloc@gmail.com>
2019-02-26 13:55:13 +0200

    Reintroduce page_size control

--- webapp/app/subjects/controller.js
+++ webapp/app/subjects/controller.js
@@ -1,15 +1,15 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 
 import config from "../config/environment";
 
 export default Controller.extend({
-  available_page_sizes: config.APP.available_page_sizes,
   page: 1,
   page_size: config.APP.default_page_size,
+  available_page_sizes: config.APP.available_page_sizes,
 
   queryParams: ["sort", "page", "page_size"],
 
   sort: "last_activity",
 
-  sort_options: ["last_activity", "name"]
+  sort_options: ["last_activity", "name"],
 });
