ef4603b6e8b7b6415e376ab22d71b44df4de17aa
Rotem Yaari <vmalloc@gmail.com>
2019-02-26 13:55:13 +0200

    Reintroduce page_size control

--- webapp/app/components/results-nav/component.js
+++ webapp/app/components/results-nav/component.js
@@ -1,12 +1,15 @@
-import { notEmpty, gt } from '@ember/object/computed';
-import Component from '@ember/component';
+import config from "../../config/environment";
+import { notEmpty, gt } from "@ember/object/computed";
+import Component from "@ember/component";
 
 export default Component.extend({
+  available_page_sizes: config.APP.available_page_sizes,
+  classNames: "d-flex",
   page: 1,
   has_next: false,
   num_pages: null,
 
-  has_last: notEmpty('num_pages'),
+  has_last: notEmpty("num_pages"),
 
   has_prev: gt("page", 1),
 
@@ -24,7 +27,7 @@ export default Component.extend({
     },
 
     last_page() {
-      this.set('page', this.get('num_pages'));
+      this.set("page", this.get("num_pages"));
     },
-  }
+  },
 });
