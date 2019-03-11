1bf6164e6e232d4f5740441c19168fe4dd4667eb
Rotem Yaari <vmalloc@gmail.com>
2019-03-11 14:12:45 +0200

    Fix UI tests for Travis

--- integration_tests/ui/test_ui_sanity.py
+++ integration_tests/ui/test_ui_sanity.py
@@ -1,7 +1,6 @@
 def test_logout(ui):
     ui.driver.find_element_by_css_selector('button.logout').click()
-    assert ui.driver.find_element_by_css_selector('h1').text == 'Sign in Required'
-    assert ui.driver.find_element_by_id('username')
+    assert ui.driver.find_element_by_css_selector('login-form')
 
 
 def test_recorded_sessions_visible(ui_session):
