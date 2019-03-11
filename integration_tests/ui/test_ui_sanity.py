11cc02c3735187ebf2301ca3a65cf5e666a7862e
Rotem Yaari <vmalloc@gmail.com>
2019-03-11 15:58:03 +0200

    Fix Travis build

--- integration_tests/ui/test_ui_sanity.py
+++ integration_tests/ui/test_ui_sanity.py
@@ -1,6 +1,6 @@
 def test_logout(ui):
     ui.driver.find_element_by_css_selector('button.logout').click()
-    assert ui.driver.find_element_by_css_selector('login-form')
+    assert ui.driver.find_element_by_css_selector('.login-form')
 
 
 def test_recorded_sessions_visible(ui_session):
