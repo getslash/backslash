08e67ac6b175c0e89dbbfdc7bfc2deee45ce2070
Rotem Yaari <vmalloc@gmail.com>
2019-03-17 14:35:40 +0200

    Fix Travis build

--- webapp/app/components/error-box/component.js
+++ webapp/app/components/error-box/component.js
@@ -4,7 +4,7 @@ export default Component.extend({
   expanded: false,
   error: null,
 
-  classNames: "mx-3 my-2",
+  classNames: "mx-3 my-2 error-box",
   classNameBindings: ["expanded", "error.is_interruption:interruption"],
 
   actions: {
