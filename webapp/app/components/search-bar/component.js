f3a9ed4a7b0b7264a4aa962bc96e7ff29e532408
Rotem Yaari <vmalloc@gmail.com>
2019-03-24 11:08:24 +0200

    Make filter for user search clearer to understand (closes #487)

--- webapp/app/components/search-bar/component.js
+++ webapp/app/components/search-bar/component.js
@@ -1,9 +1,10 @@
-import { observer } from '@ember/object';
-import Component from '@ember/component';
+import { observer } from "@ember/object";
+import Component from "@ember/component";
 
 export default Component.extend({
-
   classNames: "nav navbar-nav",
+
+  hint: "Search for...",
 
   entered_search: null,
   search: null,
@@ -12,24 +13,24 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-    this.set('entered_search', this.get('search'));
+    this.set("entered_search", this.get("search"));
   },
 
-  on_entered_search_updated: observer('search', function() {
-    if (this.get('update_entered_search')) {
-      this.set('entered_search', this.get('search'));
+  on_entered_search_updated: observer("search", function() {
+    if (this.get("update_entered_search")) {
+      this.set("entered_search", this.get("search"));
     }
   }),
 
   actions: {
     search() {
       let entered_search = this.get("entered_search");
-      if (entered_search !== this.get('search')) {
-        this.set('searching', true);
+      if (entered_search !== this.get("search")) {
+        this.set("searching", true);
       }
       this.set("update_entered_search", false);
       this.set("search", entered_search);
       this.set("update_entered_search", true);
-    }
-  }
+    },
+  },
 });
