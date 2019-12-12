6d9d0b9013ca4ae69509315b5288f8efafd21e91
Ayala Shachar <ayalas@infinidat.com>
2019-12-12 10:19:20 +0200

    Change ProxyFix import path (changed in werkzeug)

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,7 +6,7 @@ from flask_mail import Mail
 import logbook
 import logging
 from logbook.compat import redirect_logging
-from werkzeug.contrib.fixers import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 from raven.contrib.flask import Sentry
 from werkzeug.exceptions import HTTPException
 
