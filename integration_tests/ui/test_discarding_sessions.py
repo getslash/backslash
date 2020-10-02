dbe7b2a8664320f4cdb9d97207b6cbb296576641
Rotem Yaari <vmalloc@gmail.com>
2020-10-02 20:43:13 +0300

    Fix integration tests for discarding sessions

--- integration_tests/ui/test_discarding_sessions.py
+++ integration_tests/ui/test_discarding_sessions.py
@@ -1,5 +1,7 @@
 import time
 
+from selenium.webdriver.common.alert import Alert
+
 def test_session_has_discard_button(ui, ui_session):
     ui_session.click()
     ui.driver.find_element_by_css_selector('.discard-btn')
@@ -12,6 +14,9 @@ def test_session_discard_undo(ui, ui_session): # pylint: disable=unused-argument
     ui_session.click()
     button = ui.driver.find_element_by_css_selector('.discard-btn')
     button.click()
+    alert = Alert(ui.driver)
+    alert.send_keys("30")
+    alert.accept()
     undo_button = ui.driver.find_element_by_css_selector('button.undo-discard-btn')
     ui.assert_no_element('.discard-btn')
     undo_button.click()
